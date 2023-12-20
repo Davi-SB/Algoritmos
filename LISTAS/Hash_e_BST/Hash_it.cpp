7c283f19b2bca7b87a665a2c2b094e50245f7c63
Davi <davibrilhante0102@gmail.com>
2023-12-20 01:19:25 -0300

    Hashit unsolved

--- LISTAS/Hash_e_BST/Hash_it.cpp
+++ LISTAS/Hash_e_BST/Hash_it.cpp
@@ -2,13 +2,14 @@
 #include <string>
 using namespace std;
 
+// https://www.spoj.com/problems/HASHIT/
+
 #define endl '\n'
 #define TABLE_SIZE 101
 
 typedef struct {
     pair<int, string> data;
     bool occupied; // se ocupado, nao inserir (insert). diz se o valor eh relevante
-    bool unsealed; // se deslacrado, continuar procurando. uma vez deslacrado, nunca mais volta a ser como era antes. 
 } TableElement;
 
 typedef struct {
@@ -50,7 +51,7 @@ void QuickSort(pair<int, string> arr[], int left, int right) {
 HashTable* InitHashTable() {
     HashTable* ht = new HashTable;
     for (int i = 0; i < TABLE_SIZE; ++i) { // inicializa todos os occupied como false
-        ht->table[i].occupied = ht->table[i].unsealed = false;
+        ht->table[i].occupied = false;
     }
     ht->maxSize = TABLE_SIZE;
     ht->count = 0;
@@ -60,37 +61,38 @@ HashTable* InitHashTable() {
 int HashFunction(string element) {
     int sum=0;
     for (int i = 0; i < static_cast<int>(element.size()); i++) {
-        sum += ((element[i] - '0') * (i+1));
+        sum += (element[i] * (i+1));
     }
     return abs(sum*19) % TABLE_SIZE;
 }
 
 int Find(HashTable* ht, string element, int index) { // retorna o indice da key no array de table se encontrar. se nao, retorna -1
-    int j=1;
+    int j=1, saved = index;
     while(ht->table[index].occupied && (ht->table[index].data.second != element) && (j < 20)) {
-        index = ((index+(j*j)+(23*j)) % TABLE_SIZE);
+        index = ((saved+(j*j)+(23*j)) % TABLE_SIZE);
         j++;
     }
 
-    if(ht->table[index].occupied && (j < 20)) return index; // para o element ter sido mesmo achado, saiu do while unicamente por causa da condicao (!= element)
+    if(ht->table[index].occupied && (ht->table[index].data.second == element)) return index; // para o element ter sido mesmo achado, saiu do while unicamente por causa da condicao (!= element)
     return -1;
 }
 
 void Insert(HashTable* ht, string element) {
     int index = HashFunction(element);
-    
+    int saved = index;
+
     if((ht->count >= ht->maxSize) || (Find(ht, element, index) != -1)) return; // checa se nao esta cheio antes de inserir e se o elemento nao eh repetido
 
     int j=1;
 
     while(ht->table[index].occupied && (j < 20)) {
-        index = ((index+(j*j)+(23*j)) % TABLE_SIZE);
+        index = ((saved+(j*j)+(23*j)) % TABLE_SIZE);
         j++;
     }
 
     ht->table[index].data.first = index;
     ht->table[index].data.second = element;
-    ht->table[index].occupied = ht->table[index].unsealed = true;
+    ht->table[index].occupied = true;
     ht->count++;
 }
 
@@ -116,7 +118,6 @@ pair<int, string>* GetData(HashTable* ht) {
 
 void PrintData(pair<int, string> data[], int size) {
     for (int i = 0; i < size; i++) {
-        cout << "aqui" << endl;
         cout << data[i].first << ":" << data[i].second << endl;
     } 
 }
@@ -129,16 +130,14 @@ int main() {
     while(nTests--) { 
         HashTable* ht = InitHashTable();
         
-        cin >> nOperations; //cin.ignore();
+        cin >> nOperations; cin.ignore();
         while(nOperations--) {
             getline(cin, act, ':');
-            cin >> currKey;
+            getline(cin, currKey, '\n');
             if(act == "ADD") {
-                //cout << "ADD:_" << currKey << "_" << endl;
                 Insert(ht, currKey);
             }
             else { // DEL
-                //cout << "DEL:_" << currKey << "_" << endl;
                 Remove(ht, currKey);
             }        
         }
@@ -150,10 +149,12 @@ int main() {
         PrintData(data, ht->count); // second line
 
         act.clear();
-        delete ht;     
+        delete ht;  
+        //cout << endl; //sera????????  
     }
 
     return 0;
 }
 // g++ Hash_it.cpp -o HIT
 // g++ Hash_it.cpp -o HIT && ./HIT < input.in
+
