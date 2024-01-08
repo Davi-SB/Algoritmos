fd1d8ef4c498672c208913b047893d26c713a7f2
Davi <davibrilhante0102@gmail.com>
2024-01-07 23:04:33 -0300

    add all falta arrumar

--- LISTAS/AVL_e_Heaps/Add_All.cpp
+++ LISTAS/AVL_e_Heaps/Add_All.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define MAX_SIZE 5001
+#define endl '\n'
 
 // fazer o heapsort ao contrario
 // pega os dois menores, soma e esse resultado entra no array
@@ -53,17 +54,30 @@ void Heapify(Heap* heap, int i) {
         }
     }   
     heap->H[k] = v; // termina os "swaps"
-    if(heap->H[0] != INT_MIN) cout << "CUIDADO" << endl;
+    if(heap->H[0] != INT_MIN) {
+        cout << "CUIDADO" << endl;
+    }
 }
 
 void HeapBottomUp(Heap* heap) {
-    for (int i = (int)(heap->size/2); i >= 1; i--) Heapify(heap, i);
+    for (int i = heap->size/2; i >= 1; i--) Heapify(heap, i);
+}
+
+void HeapTopDown(Heap* heap, int k) {
+    heap->size++;
+    heap->H[heap->size] = k;
+    
+    int i = heap->size;
+    while ((i > 1) && (heap->H[i] > heap->H[(i/2)])) { //filho maior q o pai
+        swap(heap->H[i], heap->H[(i/2)]);
+        i /= 2;
+    }
 }
 
 void HeapSort(Heap* heap, int k) { // ordena os K maiores elementos
     if(k >= heap->size) k = heap->size-1;
     
-    nao esquecer//HeapBottomUp(heap);
+    //HeapBottomUp(heap);
     //PrintHeap(heap); cout << endl;
     
     int temp = heap->size;
@@ -72,7 +86,7 @@ void HeapSort(Heap* heap, int k) { // ordena os K maiores elementos
         heap->size--; // lazy deletion do maior elemento (ordenado)
         Heapify(heap, 1);
         //PrintHeap(heap);
-    } cout << endl;
+    } // cout << endl;
     heap->size = temp;
 }
 
@@ -80,24 +94,31 @@ int main() {
     int n;
     while (cin >> n && n) {
         Heap* heap = HeapInit(n+1); // +1 so por seguranca
+        int costSum = 0;
         
         for (int i = 0; i < n; i++) { // coloca os numeros no array do heap
             cin >> heap->H[i+1]; // i+1 para comecar da posicao 1 do array
-        }
+        } heap->size = n;
 
-        HeapBottomUp(heap);
+        HeapBottomUp(heap); // heapicifa pela primeira vez
         
         while(n-1) {
+            HeapSort(heap, 2); // manda os dois menores elementos para o final do array
             int minor1 = heap->H[heap->size], minor2 = heap->H[heap->size-1]; // armazena os dois menores selecionados
-            heap->H[heap->size-1] = minor1 + minor2;
-            heap->size--;
-            HeapSort(heap, 2); // o heapify deve fazer parte dele e ser o TOP DOWN nesse caso
+            heap->size = (heap->size > 2) ? (heap->size - 2) : 0; // lazy deletion dos dois menores elementos da heap
+
+            costSum += (minor1 + minor2); // acumula os pesos
+
+            HeapTopDown(heap, (minor1 + minor2)); // insere o novo elemento e heapifica o array de forma top-down
 
             n--;
         }
 
+        cout << costSum << endl;
         DeleteHeap(heap);
     }
     
     return 0;
 }
+// g++ Add_All.cpp -o AddAll
+// g++ Add_All.cpp -o AddAll && ./AddAll < input.in
