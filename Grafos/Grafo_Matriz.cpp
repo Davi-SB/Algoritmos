19d629059f20c69bc1aeff3a91688926b0fb7e74
Davi <davibrilhante0102@gmail.com>
2024-02-02 19:17:44 -0300

    matrix graph

--- Grafos/Grafo_Matriz.cpp
+++ Grafos/Grafo_Matriz.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class Graph {
@@ -6,23 +7,78 @@ private:
     int** matrix;
     int* mark;
     int numEdges, numNodes;
+    const int UNVISITED = 0, VISITED = 1;
+    void (Graph:: *traverse)(int v) = nullptr;
 
-    void pVerify(int *p)  { if(p == nullptr) { cerr << "memory error" << endl; exit(1); } }
-
-    void pVerify(int **p) { if(p == nullptr) { cerr << "memory error" << endl; exit(1); } }
-
-    int first(int v) {
-        for (int i = 0; i < (numEdges-1); i++) {
-            if(matrix[v][i] != 0) return i; // ausencia
-        }
-        return numEdges;
+    void pVerify(int *p)  { 
+        if(p == nullptr) { cerr << "memory error" << endl; exit(1); } 
     }
 
-    int next(int v, int w) { // primeiro em que V se liga apos o  vertice W
-        for (int i = (w+1); i < (numEdges-1); i++) {
-            if(matrix[v][i] != 0) return i; // ausencia
+    void pVerify(int **p) {
+        if(p == nullptr) { cerr << "memory error" << endl; exit(1); }
+    }
+
+    int first(int currNode) {
+        for (int i = 0; i < (numNodes-1); i++) {
+            if(matrix[currNode][i] != UNVISITED) return i; // ausencia
         }
-        return numEdges;
+        return numNodes;
+    }
+
+    int next(int currNode, int w) { // primeiro em que V se liga apos o  vertice W
+        for (int i = (w+1); i < (numNodes-1); i++) {
+            if(matrix[currNode][i] != UNVISITED) return i; // ausencia
+        }
+        return numNodes;
+    }
+
+    void setMark(int currNode, int state) { 
+        mark[currNode] = state;
+    }
+    
+    bool getMark(int currNode) {
+        if(mark[currNode] == UNVISITED) return false;
+        return true;
+    }
+
+    void preVisit(int currNode) { return; }
+
+    void posVisit(int currNode) { return; }
+
+    void DFS(int currNode) {
+        // preVisit(currNode);
+
+        setMark(currNode, VISITED);
+        int nextNode = first(currNode);
+
+        while(nextNode < numNodes) {
+            if(getMark(nextNode) == UNVISITED)
+                DFS(nextNode);
+            nextNode = next(currNode, nextNode);
+        }
+
+        // posVisit(currNode);
+    }
+
+    void BFS(int start) {
+        queue<int> nodeQueue;
+        nodeQueue.push(start);
+        setMark(start, VISITED);
+
+        while (nodeQueue.size() > 0) {
+            int currNode = nodeQueue.front(); nodeQueue.pop();
+            // preVisit(currNode);
+            int nextNode = first(currNode);
+
+            while (nextNode < numNodes) {
+                if(getMark(nextNode) == UNVISITED) {
+                    setMark(nextNode, VISITED);
+                    nodeQueue.push(nextNode);
+                }
+                nextNode = next(currNode, nextNode);
+            }
+            // posVisit(currNode);
+        }
     }
 
 public:
@@ -36,19 +92,33 @@ public:
         numEdges = 0;
     }
     ~Graph() {
-        for (int i = 0; i < numNodes; i++) delete[] matrix[i];
+        for (int i = 0; i < (numNodes-1); i++) delete[] matrix[i];
         delete[] matrix;
+        delete[] mark;
     }
 
     void setEdge(int i, int j, int weight) {
         if(weight == 0) { cerr << "peso nulo"; exit(1); }
-        if(matrix[i][j] == 0) numEdges++; // ausencia
+        if(matrix[i][j] == UNVISITED) numEdges++; // ausencia
         matrix[i][j] = weight;
     }
 
     void delEdge(int i, int j) {
-        if(matrix[i][j] != 0) numEdges--; // ausencia
-        matrix[i][j] = 0; // ausencia
+        if(matrix[i][j] != UNVISITED) numEdges--; // ausencia
+        matrix[i][j] = UNVISITED; // ausencia
+    }
+
+    void graphTraverse(int v, char searchType[3]) { // "DFS" or "BFS" expected
+        if(searchType[0] == 'D') traverse = DFS;
+        if(searchType[0] == 'B') traverse = BFS;
+        else { cerr << "error searchType - graphTraverse" << endl; exit(1); }
+
+        for(int i = 0; i < (numEdges-1); i++) setMark(i, UNVISITED);
+
+        for(int i = 0; i < (numEdges-1); i++) {
+            if(getMark(v) == UNVISITED) 
+                (this->*traverse)(v);
+        }
     }
 };
 
