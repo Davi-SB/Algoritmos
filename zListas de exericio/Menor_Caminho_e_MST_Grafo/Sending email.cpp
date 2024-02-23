c7d44bc3d95582c434cb33272b675f2e1c416684
dsb6 <dsb6@g5c37.cin.ufpe.br>
2024-02-23 09:53:38 -0300

    sending email solved

--- zListas de exericio/Menor_Caminho_e_MST_Grafo/Sending email.cpp
+++ zListas de exericio/Menor_Caminho_e_MST_Grafo/Sending email.cpp
@@ -2,7 +2,6 @@
 #include <limits.h>
 #include <vector>
 #include <queue>
-#include <set>
 using namespace std;
 
 class Node {
@@ -40,29 +39,24 @@ private:
     vector<Node>* graphList;
     bool* mark;
     int *distance, *parent;
-    int numEdges, numNodes;
+    int numEdges, numNodes, itNext;
     const bool VISITED = true;
-
-    void checkNode(int nodeIndex) {
-        if((nodeIndex < 0) || (nodeIndex >= this->numNodes)) { cerr << "node out of bounds - checkNode" << endl; exit(1); }
-    }
-
-    bool checkNodeIgnore(int nodeIndex) {
-        if((nodeIndex < 0) || (nodeIndex >= this->numNodes)) return true;
-        return false;
-    }
 
     Node first(int currNode) {
         if(!graphList[currNode].empty()) return graphList[currNode][0];
         return Node(numNodes, numNodes);
     }
 
-    Node next(int currNode, int w) { // primeiro em que V se liga apos o vertice W
-        if(w+1 < graphList[currNode].size()) return graphList[currNode][w+1];
+    Node next(int currNode) {
+        if(itNext+1 < graphList[currNode].size()) {
+            itNext++;
+            return graphList[currNode][itNext];
+        }
+        itNext = 0;
         return Node(numNodes, numNodes);
     }
 
-    void dijkstra(int start) {
+    void dijkstra(int start, int destination) {
         for (int i = 0; i < numNodes; i++) { // inicializacao
             distance[i] = INT_MAX;
             parent[i] = -1;
@@ -80,6 +74,7 @@ private:
                 t = pq.top(); pq.pop();
             } while(mark[t.curr] == VISITED);
             mark[t.curr] = VISITED;
+            if(mark[destination]) return;
             parent[t.curr] = t.prev;
 
             Node n = first(t.curr);
@@ -88,22 +83,9 @@ private:
                     distance[n.index] = distance[t.curr] + n.weight;
                     pq.push(Trio(t.curr, n.index, distance[n.index]));
                 }
-                n = next(t.curr, n.index);
-                cout << "chegou" << endl;
+                n = next(t.curr);
             }
         }
-    }
-
-    vector<Node> removeRepeated(vector<Node> vec) {
-        set<Node> newSet;
-        vector<Node> resultado;
-        for (Node elemento : vec) {
-            if (newSet.find(elemento) == newSet.end()) {
-                newSet.insert(elemento);
-                resultado.push_back(elemento);
-            }
-        }
-        return resultado;
     }
 
 public:
@@ -114,6 +96,7 @@ public:
         this->parent    = new int[size];
         this->numNodes  = size;
         this->numEdges  = 0;
+        this->itNext = 0;
     }
     ~Graph() {
         delete[] this->graphList;
@@ -123,20 +106,12 @@ public:
     }
 
     void setEdge(int a, int b, int weight) {
-        if (checkNodeIgnore(a) || checkNodeIgnore(b)) return;
-        Node temp(b, weight);
-        graphList[a].push_back(temp);
+        graphList[a].push_back(Node(b, weight));
         numEdges++;
     }
 
-    void reduceGraph() {
-        for (int i = 0; i < numNodes; i++) {
-            graphList[i] = removeRepeated(graphList[i]);
-        } 
-    }
-
     int getDistance(int start, int destination) {
-        dijkstra(start);
+        dijkstra(start, destination);
         return distance[destination];
     }
 
@@ -153,7 +128,6 @@ int main () {
             servers.setEdge(i, j, weight);
             servers.setEdge(j, i, weight);
         }
-        // servers.reduceGraph();
         int distance = servers.getDistance(origin, destination);
         cout << "Case #" << i+1 << ": ";
         if(distance != INT_MAX) cout << distance << endl;
