375d1cb92ab1d8825cf49cf22733d856b184c411
dsb6 <dsb6@g5c34.cin.ufpe.br>
2024-04-26 13:56:22 -0300

    sorts limpos

--- Sorts_e_BSearch/MergeSort/MergeSortOMelhor.cpp
+++ Sorts_e_BSearch/MergeSort/MergeSortOMelhor.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void Merge(int arr[], int left, int right, int size) {
+void Merge(int arr[], int l, int r, int size) {
     // obs: se atentar com a inicializacao e condicao de parada dos for dessa funcao    
-    int mid = (int)((left + right) / 2); // metade do subarrray
-    int index1 = left, index2 = mid+1; // index inicial de cada um dos dois subarrays. sub1[left..mid], sub2[mid+1..right]
+    int m = (int)((l + r) / 2); // metade do subarrray
+    int i1 = l, i2 = m+1; // index inicial de cada um dos dois subarrays. sub1[l..m], sub2[m+1..r]
     int temp[size];
 
-    for (int i = left; i <= right; i++){ temp[i] = arr[i]; } // copia os elementos em questão de arr para temp na mesma posicao
+    for (int i = l; i <= r; i++){ temp[i] = arr[i]; } // copia os elementos em questão de arr para temp na mesma posicao
     
-    for (int curr = left; curr <= right; curr++) {
-        if(index1 > mid) arr[curr] = temp[index2++]; // atribui o resto. (index1 > mid) significa que index1 invadiu sub2
-        else if(index2 > right) arr[curr] = temp[index1++]; // atribui o resto. (index2 > right) significa que index2 ja esgotou sub2
+    for (int curr = l; curr <= r; curr++) {
+        if(i1 > m)      arr[curr] = temp[i2++]; // atribui o resto. (i1 > m) significa que i1 invadiu sub2
+        else if(i2 > r) arr[curr] = temp[i1++]; // atribui o resto. (i2 > r) significa que i2 ja esgotou sub2
 
-        else if(temp[index1] <= temp[index2]) arr[curr] = temp[index1++];
-        else arr[curr] = temp[index2++];
-    }   
+        else if(temp[i1] <= temp[i2]) arr[curr] = temp[i1++];
+        else                          arr[curr] = temp[i2++];
+    }
 }
 
-void MergeSort(int arr[], int left, int right, int size) {
-    if(left < right) {
-        int mid = (int)((left + right) / 2); // metade do subarrray
+void MergeSort(int arr[], int l, int r, int size) {
+    if(l < r) {
+        int m = (int)((l + r) / 2); // metade do subarrray
+        MergeSort(arr, l, m, size); // m-1 daria problema quando m == 0
+        MergeSort(arr, m+1, r, size);
 
-        MergeSort(arr, left, mid, size); // mid-1 daria problema quando m == 0
-        MergeSort(arr, mid+1, right, size);
-
-        Merge(arr, left, right, size);
+        Merge(arr, l, r, size);
     }
 }
 
--- Sorts_e_BSearch/QuickSort.cpp
+++ Sorts_e_BSearch/QuickSort.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void SwapInt(int arr[], int i, int j) {
-    int aux = arr[i]; arr[i] = arr[j]; arr[j] = aux;
-}
-
 int Partition(int arr[], int left, int right) {
-    int pivot = arr[left];
-    int i = left, j = right+1;
+    int pivot = arr[left], i = left, j = right+1;
 
     do {
-        do {
-            i++;
-        } while((arr[i] < pivot) && (i < right)); // enquanto os elementos mais da *esquerda* forem *menores* que o pivot        
-        
-        do {
-            j--;
-        } while(arr[j] > pivot); // enquanto os elementos mais da *direita* forem *maiores* que o pivot
+        do { i++; } while((arr[i] < pivot) && (i < right)); // enquanto os elementos mais da *esquerda* forem *menores* que o pivot        
+        do { j--; } while(arr[j] > pivot); // enquanto os elementos mais da *direita* forem *maiores* que o pivot
 
-        SwapInt(arr, i, j); // swap arr[i] e arr[j]
+        swap(arr[i], arr[j]);
     } while(i < j);
 
-    SwapInt(arr, i, j); // undo-swap arr[i] e arr[j] // desfazer ultimo swap. esse eh sempre um excesso. quando i>=j
-    SwapInt(arr, left, j); // swap arr[left] e arr[j]. manda o pivot pra posicao correta
+    swap(arr[i], arr[j]); // undo-swap arr[i] e arr[j] // desfazer ultimo swap. esse eh sempre um excesso. quando i>=j
+    swap(arr[left], arr[j]); // manda o pivot pra posicao correta
     return j; // index do pivot ao fim da particao, a poosicao correta que ele ja esta
 }
 
 void QuickSort(int arr[], int left, int right) {
     if(left < right) {
         int s = Partition(arr, left, right);
-
         QuickSort(arr, left, s-1);
         QuickSort(arr, s+1, right);
     }
